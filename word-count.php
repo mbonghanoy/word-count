c3dad81719611cf40c29ef44c89d189e2da6607c
mbonghanoy <bonghanoymark@gmail.com>
2017-07-27 17:45:31 +0800

    started count words with loop

--- word-count.php
+++ word-count.php
@@ -4,10 +4,6 @@ function wordCount($textFile)
 {
     $replaced = str_replace(['?',";",',','`','-','!',"'",')','(','.',':','"']," ",$textFile);
     $loweredCase = strtolower($replaced);
-    $count = array_count_values(str_word_count($loweredCase, 1));
-    arsort($count);
-    $top = array_slice($count, 0, 10);
-    print_r($top);
 }
 
 $alice = file_get_contents('alice.txt');
